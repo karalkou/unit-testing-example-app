3842d1caf33cc539c9c240d85caa12a04a817f96
Siarhei Saroka <soroka.sergei@gmail.com>
2018-03-26 22:44:30 +0300

    step 1: end

--- test/getEmojis.spec.js
+++ test/getEmojis.spec.js
@@ -2,4 +2,15 @@ const getEmojis = require("../lib/getEmojis");
 var assert = require("assert");
 
 describe("getEmojis", () => {
+    it("should retun an array", () => {
+        const emojiList = getEmojis();
+
+        assert(Array.isArray(emojiList), "did not return an array");
+    });
+
+    it("should retun an array with 7 items", () => {
+        const emojiList = getEmojis();
+
+        assert.equal(emojiList.length, 7, "did not return 7 items");
+    });
 });
