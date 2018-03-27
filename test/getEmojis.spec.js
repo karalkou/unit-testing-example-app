9b73000343992af3f5fc04439744139262a66991
Siarhei Saroka <soroka.sergei@gmail.com>
2018-03-28 00:00:38 +0300

    step 10

--- test/getEmojis.spec.js
+++ test/getEmojis.spec.js
@@ -1,25 +1,24 @@
 const getEmojis = require("../lib/getEmojis");
-var assert = require("chai").assert;
+const should = require("chai").should();
 
 describe("getEmojis", () => {
-    it("should throw an error if result is not an array", (done) => {
+    it("should throw an error if result is not an array", () => {
         const notArray = () => {
             getEmojis(process.cwd() + "/test/fixtures/emoji-non-array.json");
         };
 
-        assert.throws(notArray, /is not an array/);
-        done();
+        should.Throw(notArray, /is not an array/);
     });
 
     it("should retun an array", () => {
         const emojiList = getEmojis();
 
-        assert.isArray(emojiList, "did not return an array");
+        emojiList.should.be.an("array");
     });
 
     it("should retun an array with 7 items", () => {
         const emojiList = getEmojis();
 
-        assert.lengthOf(emojiList, 7, "did not return 7 items");
+        emojiList.should.be.an("array").with.length(7);
     });
 });
