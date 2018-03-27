a5f709b4538e0f4780acc7dc6032fcba885876f8
Siarhei Saroka <soroka.sergei@gmail.com>
2018-03-28 00:44:26 +0300

    step 13: end

--- test/hexToRgb.spec.js
+++ test/hexToRgb.spec.js
@@ -1,25 +1,35 @@
-let hexToRgb = require("../lib/hexToRgb");
-let assert = require("chai").assert;
+var hexToRgb = require("../lib/hexToRgb");
+var expect = require("chai").expect;
+var sinon = require("sinon");
 
-describe("hexToRgb", () => {
-    it("should return an error when value is not a hex code", (done) => {
-        hexToRgb("blue", (error, result) => {
-            assert(error);
-            done();
-        });
+describe.only("hexToRgb", () => {
+  describe("convert method", () => {
+    it("should call parse once", (done) => {
+      sinon.spy(hexToRgb, "parse");
+
+      hexToRgb.convert("#ffffff", (err, result) => {
+        expect(hexToRgb.parse.calledOnce).to.be.true;
+        expect(hexToRgb.parse.args[0][0]).to.have.length(6);
+
+        hexToRgb.parse.restore();
+        done();
+      });
     });
 
-    it("should convert hex to rgb", (done) => {
-        const hex = {
-            r: 255,
-            g: 255,
-            b: 255,
-        };
-
-        hexToRgb("#FFF", (error, rgb) => {
-            assert.strictEqual(error, null);
-            assert.deepEqual(rgb, hex);
-            done();
-        });
+    it("should return an error if the value is not a hex code", (done) => {
+      hexToRgb.convert("blue", (error, result) => {
+        expect(error).to.exist;
+        done();
+      });
     });
+
+    it("should return a correctly converted rgb value", (done) => {
+      hexToRgb.convert("#fff", (error, result) => {
+        expect(error).to.not.exist;
+        expect(result).to.deep.equal({ r: 255, g: 255, b:255});
+
+        done();
+      });
+    });
+  });
 });
