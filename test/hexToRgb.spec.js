a97b59ce000efc009dc53a1e3f249dcae8a990cf
Siarhei Saroka <soroka.sergei@gmail.com>
2018-03-27 22:14:03 +0300

    step 5: end

--- test/hexToRgb.spec.js
+++ test/hexToRgb.spec.js
@@ -2,19 +2,24 @@ let hexToRgb = require("../lib/hexToRgb");
 let assert = require("assert");
 
 describe("hexToRgb", () => {
-    it("should throw an error when value is not a hex code", () => {
-        assert.throws(() => { hexToRgb("blue"); }, /Invalid hex argument/);
+    it("should return an error when value is not a hex code", (done) => {
+        hexToRgb("blue", (error, result) => {
+            assert(error);
+            done();
+        });
     });
 
-    it("should convert hex to rgb", () => {
+    it("should convert hex to rgb", (done) => {
         const hex = {
             r: 255,
             g: 255,
             b: 255,
         };
 
-        const rgb = hexToRgb("#FFF");
-
-        assert.deepEqual(rgb, hex)
+        hexToRgb("#FFF", (error, rgb) => {
+            assert.strictEqual(error, null);
+            assert.deepEqual(rgb, hex);
+            done();
+        });
     });
 });
